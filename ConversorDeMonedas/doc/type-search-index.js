907a5e49c289dfbbea44c612efd5bc645163a32e
GaroHurtado <ing.ehurtado@outlook.com>
2023-03-01 12:11:24 -0600

    Actualizando

--- ConversorDeMonedas/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Conversor"},{"p":"<Unnamed>","l":"prueba"},{"p":"<Unnamed>","l":"pruebaHiper"}];updateSearchResults();
